9048c6e136ae0431197da96647fa3b6fc7daa126
Jason Pavlas <jasonpavlas@jasons-mbp.lan>
2022-10-05 12:03:41 -0400

    added crispy plugin for views

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
+#from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
+    #path('login/', auth_views.LoginView.as_view(), name='login'),
+    #path('logout/', auth_views.LoginView.as_view(), name='logout'),
     path('', include('blog.urls')),
 ]
