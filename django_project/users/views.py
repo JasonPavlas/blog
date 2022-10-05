258fdbe1d00473c24fd3546d4182c52f0c4ae9e6
Jason Pavlas <jasonpavlas@jasons-mbp.lan>
2022-10-05 11:41:05 -0400

    added user validation

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, redirect 
-from django.contrib.auth.forms  import UserCreationForm 
+from django.contrib.auth.forms import UserCreationForm 
 from django.contrib import messages
 
-        ###this definition maps to a file in the users/remplates/users directory
-        ###(request) are the hppt requests from the browser 
+       
 def register(request):
+    """a stab at the docstring error"""
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            username = form.cleanded_data.get('username')
+            username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
